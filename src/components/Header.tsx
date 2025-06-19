38334fed0e385c1e32d2ec265f49330a918086af
AmanDevelops <amanpal2072004@gmail.com>
2025-06-19 16:22:19 +0530

    Add SVG assets, update Header component, enhance Main component with loading and error states, and apply Nunito font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 function Header() {
   return (
     <nav>
-      <span className="nav-title">SMS Wiki</span>
+      <div className="title">
+        <img src="/favicon.ico" alt="icon" />
+        <span className="nav-title">SMS Wiki</span>
+      </div>
       <div className="nav-links">
         <a href="/">Home</a>
         <a href="https://github.com/AmanDevelops">Github</a>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+import NotFound from "../assets/404.svg";
+import ExternalLink from "../assets/external-link.png";
+import HeaderInfo from "../assets/HEADER INFO.svg";
+
 function Main() {
   const [header, setHeader] = useState<string>("");
   const [name, setName] = useState<string>("");
@@ -12,7 +16,7 @@ function Main() {
     setStatus("searching");
     e.preventDefault();
 
-    if (!header.trim()){
+    if (!header.trim()) {
       setStatus("noInput");
       return;
     }
@@ -35,23 +39,173 @@ function Main() {
     <main>
       <div className="container">
         <h1>SMS Directory</h1>
+        <img src={HeaderInfo} alt="headerinfo" className="info-image" />
         <form onSubmit={handleSearch}>
           <input
             type="text"
             value={header}
             onChange={(e) => setHeader(e.target.value)}
+            placeholder="NSESMS"
           />
           <input type="submit" disabled={status === "searching"} />
         </form>
-        {status === "searching" && <h3 className="searching">Searching...</h3>}
+        {status === "searching" && (
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              width="200px"
+              height="200px"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="xMidYMid"
+            >
+              <g>
+                <circle cx="73.801" cy="68.263" fill="#e15b64" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="0s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="68.263" cy="73.801" fill="#f47e60" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.062s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="61.481" cy="77.716" fill="#f8b26a" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.125s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="53.916" cy="79.743" fill="#abbd81" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.187s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="46.084" cy="79.743" fill="#849b87" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.25s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="38.519" cy="77.716" fill="#6492ac" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.312s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="31.737" cy="73.801" fill="#637cb5" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.375s"
+                  ></animateTransform>
+                </circle>
+                <circle cx="26.199" cy="68.263" fill="#6a63b6" r="3">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    calcMode="spline"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                    keySplines="0.5 0 0.5 1"
+                    repeatCount="indefinite"
+                    dur="1.4925373134328357s"
+                    begin="-0.437s"
+                  ></animateTransform>
+                </circle>
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  calcMode="spline"
+                  values="0 50 50;0 50 50"
+                  keyTimes="0;1"
+                  keySplines="0.5 0 0.5 1"
+                  repeatCount="indefinite"
+                  dur="1.4925373134328357s"
+                ></animateTransform>
+              </g>
+            </svg>
+          </div>
+        )}
         {status === "success" && (
-          <h2 style={{ textAlign: "center" }}>
-            The Company Name is <br />
-            {name}
+          <>
+            <h2 style={{ textAlign: "center", color: "green" }}>
+              The Registered Principal Entity is <br />
+              <a
+                href={`https://www.google.com/search?q=${name}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                {name} <img src={ExternalLink} alt="" />
+              </a>
+            </h2>
+          </>
+        )}
+        {status === "error" && (
+          <div className="error-container">
+            <h2 style={{ textAlign: "center", color: "red" }}>
+              Entity Not Registered or Data Not Updated
+            </h2>
+            <img src={NotFound} alt="Not Found" className="not-found-image" />
+          </div>
+        )}
+        {status === "noInput" && (
+          <h2 style={{ textAlign: "center", color: "red" }}>
+            Please Provide an Input
           </h2>
         )}
-        {status === "error" && <h3>No Results Found</h3>}
-        {status === "noInput" && <h3>Please Provide an Input</h3>}
       </div>
     </main>
   );
